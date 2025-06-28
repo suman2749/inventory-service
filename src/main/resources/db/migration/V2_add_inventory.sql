04f50ab8aef1ea9a9e91778ec7aa1927bed6a83f
Suman <suman.sde1922@gmail.com>
2025-06-28 18:10:11 +0530

    first commit

---
+++ src/main/resources/db/migration/V2_add_inventory.sql
@@ -0,0 +1,11 @@
+insert into t_inventory(quantity, sku_code)
+values (100, 'iphone-14'),
+       (200, 'macbook-pro-16'),
+       (300, 'ipad-air-5'),
+       (400, 'apple-watch-series-7'),
+       (500, 'airpods-pro-2nd-gen'),
+       (600, 'apple-tv-4k'),
+       (700, 'magic-keyboard'),
+       (800, 'magic-mouse'),
+       (900, 'homepod-mini'),
+       (1000, 'apple-cable-usb-c');
